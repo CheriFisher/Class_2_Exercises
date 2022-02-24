6540df61bd29d562108d479a0cdca312aa53fe26
CheriFisher <99698783+CheriFisher@users.noreply.github.com>
2022-02-24 10:31:50 -0600

    Add: Create JS variables/Ex.1

---
+++ Class_2_Exercises/Exercise_1/main.js
@@ -0,0 +1,24 @@
+/*let myName = "Cheri";
+console.log(myName);
+
+let num = 44;
+console.log(num);
+
+let willSleet = true;
+let work = null;
+let color;
+*/
+let car = {
+    make: "Honda",
+    model: "HRV",
+    year: "2021"
+};
+
+console.log(car);
+
+/*const sodas = ["Coke", "Pepsi", "Sprite", "RC", "Root Beer"];
+console.log(sodas[2]);
+
+const x;
+*/
+
